cc66ccd5ce4a533ef1e5d1e6ffdd9a84dc80ab01
Aswin Shibu <aswinshibu08@gmail.com>
2025-03-21 15:13:12 +0530

    final updates added

--- chain.py
+++ chain.py
@@ -8,7 +8,7 @@ import json
 class Automation:
     def __init__(self,filename):
         self.model = ChatGroq(model="gemma2-9b-it") #mistral-saba-24b
-        self.count=0
+        
         with open(filename, "r", encoding="utf-8") as f:
             self.website_content = f.read()
 
